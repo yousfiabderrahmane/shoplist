6d0e374895df867407abb48b60f89bcf522448cc
Yousfi Abderrahmane <bdrrhmnyousfi@gmail.com>
2023-02-06 09:13:03 +0100

    Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BsCart4 } from "react-icons/bs";
+
+export const Navbar = () => {
+  return (
+    <div>
+      <h1>
+        <span>
+          <BsCart4 />
+        </span>
+        Shopping List
+      </h1>
+    </div>
+  );
+};
