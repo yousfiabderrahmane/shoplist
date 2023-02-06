76c0e941135275a08e2027d24f2e4fcb523f9730
Yousfi Abderrahmane <bdrrhmnyousfi@gmail.com>
2023-02-06 09:12:34 +0100

    Setting up the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import "./App.css";
+import { Dashboard } from "./components/Dashboard";
+import { Inputs } from "./components/Inputs";
+import { Navbar } from "./components/Navbar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Inputs />
+      <Dashboard />
+    </div>
+  );
 }
 
 export default App;
